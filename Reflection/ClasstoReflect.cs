6796b03246c84d67fc76ee62e0405c6730efd83d
Sandra Tanzberger <sandraewell@hotmail.com>
2022-08-23 12:32:19 -0600

    c# Reflection demo for CTA project using attributes with factory pattern.

---
+++ Reflection/ClasstoReflect.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Reflection
+{
+	/// <summary>
+	/// Summary description for ClasstoReflect.
+	/// </summary>
+	public class ClasstoReflect
+	{
+		public ClasstoReflect()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+		public string ReflectMethod()
+		{
+			return "Method has been reflected";
+		}
+	}
+}
